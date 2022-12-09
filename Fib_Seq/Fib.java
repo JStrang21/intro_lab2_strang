87d3ac8cd1a5a2b14910fa4fcfcffd23e3b4185f
Justin Strang <Justinstrang100@gmail.com>
2022-12-09 15:48:28 -0500

    first commit

---
+++ Fib_Seq/Fib.java
@@ -0,0 +1,18 @@
+package Fib_Seq;
+
+public class Fib {
+    public static int fibSeq(int i) {
+        if (i == 0 || i == 1) {
+            return 1;
+        }
+        else {
+            return fibSeq(i - 2) + fibSeq(i - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int num = 6;
+        int result = fibSeq(num);
+        System.out.println(result);
+    }
+}
